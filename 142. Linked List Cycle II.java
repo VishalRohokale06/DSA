88a47aced3559229c8896ff185653ad73889d10c
VishalRohokale06 <vvrohokale16@gmail.com>
2026-01-03 14:57:42 +0530

    142. Linked List Cycle II

---
+++ 142. Linked List Cycle II.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+
+	    ListNode fast=head;
+	    ListNode slow=head;
+
+	    while(fast!=null && fast.next!=null){
+		    fast=fast.next.next;
+		    slow=slow.next;
+
+		    if(fast==slow){
+			    slow=head;
+			    while(fast!=slow){
+				    fast=fast.next;
+				    slow=slow.next;
+			    }
+			    return slow;
+		    }
+	    }
+	    return slow;
+    }
+}
