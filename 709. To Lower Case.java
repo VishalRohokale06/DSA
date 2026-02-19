fbd49da71cb4db6da3cd69d632769dcbadf99371
VishalRohokale06 <vvrohokale16@gmail.com>
2026-02-19 14:46:36 +0530

    709. To Lower Case.java

---
+++ 709. To Lower Case.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+
+        // StringBuilder sb = new StringBuilder();
+
+        // for(char c:s.toCharArray()){
+        //     if(c>='A' && c<='Z'){
+        //         c=(char)(c+32);
+
+        //     }
+        //     sb.append(c);
+        // }
+        // return sb.toString();
+    }
+}
