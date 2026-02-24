a957347695f72b4a5fbed72d13adc494f044c936
VishalRohokale06 <vvrohokale16@gmail.com>
2026-02-24 08:26:57 +0530

    925. Long Pressed Name

--- 925. Long Pressed Name.java
+++ 925. Long Pressed Name.java
@@ -1,19 +1,17 @@
 class Solution {
     public boolean isLongPressedName(String name, String typed) {
-        int freq1[] = new int[26];
-        int freq2[] = new int[26];
+        int i=0,j=0;
 
-        for(char c:name.toCharArray()){
-            freq1[c-'a']++;
+        while(j<typed.length()){
+            if(i<name.length() && name.charAt(i)==typed.charAt(j)){
+                i++;
+                j++;
+            }else if(j>0 && typed.charAt(j)==typed.charAt(j-1)){
+                j++;
+            }else{
+                return false;
+            }
         }
-
-        for(char c:typed.toCharArray()){
-            freq2[c-'a']++;
-        }
-
-        for(int i=0;i<26;i++){
-            if(freq2[i]<freq1[i]) return false;
-        }
-        return true;
+        return i==name.length();
     }
 }
