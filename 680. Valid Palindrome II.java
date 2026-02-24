7bfda2f6e38c50af417d6d9402c06d802fe00bd5
VishalRohokale06 <vvrohokale16@gmail.com>
2026-02-24 11:07:20 +0530

    680. Valid Palindrome II

---
+++ 680. Valid Palindrome II.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int i=0,j=s.length()-1;
+
+        while(i<j){
+            if(s.charAt(i)==s.charAt(j)){
+                i++;
+                j--;
+            }else{
+                return isPalidrome(s,i+1,j) || isPalidrome(s,i,j-1);
+            }
+        }
+        return true;
+    }
+
+    private boolean isPalidrome(String s,int i,int j){
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)) return false;
+            else{
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+}
