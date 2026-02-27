eb7a652afd6940bd2008d4f5e2d04ad4b10ae775
VishalRohokale06 <vvrohokale16@gmail.com>
2026-02-27 10:11:52 +0530

    1898. Maximum Number of Removable Characters.

---
+++ 1898. Maximum Number of Removable Characters.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int maximumRemovals(String s, String p, int[] removable) {
+        
+        int left=0;
+        int right=removable.length;
+        int ans=0;
+
+        while(left<=right){
+            int mid = left+(right-left)/2;
+
+            if(isValid(s,p,removable,mid)){
+                ans=mid;
+                left=mid+1;
+            }else{
+                right=mid-1;
+            }
+        }
+        return ans;
+    }
+
+    private boolean isValid(String s,String p,int[] rem,int mid){
+        boolean removed[] = new boolean[s.length()];
+        for(int i=0;i<mid;i++){
+            removed[rem[i]]=true;
+        }
+
+        int j=0;
+        for(int i=0;i<s.length() && j<p.length();i++){
+            if(!removed[i] && s.charAt(i)==p.charAt(j)){
+                j++;
+            }
+        }
+        return j==p.length();
+    }
+}
